181b0b13aee17ac0f1729bfd4f9aaca6d0a14af4
Matthew Cohen <matt@mattbsg.xyz>
2019-07-03 01:55:52 -0400

    Ditch caching for a db sync at start only

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ mclient = pymongo.MongoClient(
 	username=config.mongoUser,
 	password=config.mongoPass
 )
-activityStatus = discord.Activity(type=discord.ActivityType.playing, name='with Fils-A-Mech')
+activityStatus = discord.Activity(type=discord.ActivityType.playing, name='bot dev with MattBSG')
 bot = commands.Bot('()', max_messages=30000, fetch_offline_members=True, activity=activityStatus)
 
 LOG_FORMAT = '%(levelname)s [%(asctime)s]: %(message)s'
@@ -29,101 +29,28 @@ READY = False
 preCache = []
 userCache = []
 
+startTime = int(time.time())
+
 async def safe_send_message(channel, content=None, embeds=None):
     await channel.send(content, embed=embeds)
 
-@tasks.loop(seconds=15.0)
-async def stats_update():
-    global userCache
-    global preCache
-    logging.info('[Cache Tool] Running cache update')
-    updateCache = []
-    if not READY:
-        logging.info('[Cache Tool] Bot not READY. Skipping cache check')
-        return
-
-    for item in userCache:
-        if item not in preCache: # Something that is not the same
-            logging.info(f"[Cache Tool] Updating user db for {item['_id']}")
-            updateCache.append(item)
-            db = mclient.fil.users
-            doc = db.find_one({'_id': item['_id']})
-            if not doc:
-                logging.error(f"[Cache Tool] Unexpected user {item['_id']}in cache, creating")
-                db.insert_one(item)
-                continue
-
-            db.update_one({'_id': item['_id']}, {'$set': item})
-            preCache = copy.deepcopy(userCache)
-
-    logging.info('[Cache Tool] Done')
-
-async def db_cache_merge(member, guild, data): # TODO Merge newEntry with dbEntry. They are now the same
+async def store_user(member, messages=0):
     db = mclient.fil.users
-    dbUser = db.find_one({'_id': member.id})
-
-    warnTier1 = config.warnTier1
-    warnTier2 = config.warnTier2
-    warnTier3 = config.warnTier3
-    muteRole = config.mute
-    logging.debug(f'Evaluating starting cache for {member.id}')
-
-    if not dbUser:
-        # We don't have a record yet, make and toss it back
-        dbEntry = {
-            '_id': member.id,
-            'messages': data['messages'],
-            'last_message': data['last_message'],
-            'roles': data['roles'],
-            'punishments': data['punishments']
-        }
-        return data, dbEntry
-    
-    newEntry = {}
-    newEntry['_id'] = member.id
-    newEntry['messages'] = dbUser['messages']
-    newEntry['last_message'] = dbUser['last_message']
-    if Counter(dbUser['roles']) != Counter(data['roles']):
-        db.update_one({'_id': member.id}, {'$set': {
-            'roles': data['roles']
-        }})
-    
-    newEntry['roles'] = data['roles']
-    newEntry['punishments'] = dbUser['punishments']
-    if not newEntry['punishments']:
-        updateRequired = False
-        for pun in newEntry['punishments']:
-            if pun['active']:
-                if pun['type'] == 'tier1' and warnTier1 not in newEntry['roles']:
-                    updateRequired = True
-                    newEntry['roles'].append(warnTier1)
-
-                elif pun['type'] == 'tier2' and warnTier2 not in newEntry['roles']:
-                    updateRequired = True
-                    newEntry['roles'].append(warnTier2)
-
-                elif pun['type'] == 'tier3' and warnTier3 not in newEntry['roles']:
-                    updateRequired = True
-                    newEntry['roles'].append(warnTier3)
-
-                elif pun['type'] == 'mute' and muteRole not in newEntry['roles']:
-                    updateRequired = True
-                    newEntry['roles'].append(muteRole)
-
-        if updateRequired:
-            newRoles = []
-            for role in newEntry['roles']:
-                n = guild.get_role(role)
-                if not n:
-                    # discord.py failed to get role and returned None. Ignore it.
-                    logging.error(f'Unable to get role with ID {role}')
-                    continue
-                
-                newRoles.append(n)
-
-            await member.edit(roles=newRoles)
+    roleList = []
+    for role in member.roles:
+        if role.id == member.guild.id:
+            continue
         
-        return newEntry, None
+        roleList.append(role.id)
+
+    userData = {
+        '_id': member.id,
+        'messages': messages,
+        'last_message': None,
+        'roles': roleList,
+        'punishments': []
+    }
+    db.insert_one(userData)
 
 @bot.event
 async def on_ready():
@@ -140,43 +67,37 @@ async def on_ready():
     if not READY:
         bot.load_extension('jishaku')
         bot.load_extension('cogs.moderation')
-        stats_update.start() # TODO: Workaround list(dict()) not being hashable
+        bot.load_extension('cogs.utility')
 
         NS = bot.get_guild(238080556708003851)
-        dbQueue = []
 
-        logging.info('Starting cache population')
-        async def cache_fetch_loop():
-            guildSize = len(NS.members)
-            memberNumber = 0
-            for member in NS.members:
-                memberNumber += 1
-                logging.info(f'Populating member {memberNumber}/{guildSize} into cache')
-                serverData = {
-                    'messages': 0,
-                    'last_message': None,
-                    'roles': [x.id for x in member.roles],
-                    'punishments': []
-                    }
-                cacheData, dbData = await db_cache_merge(member, NS, serverData)
-                userCache.append(dbData)
-                if not dbData:
-                    # This is an update
-                    db.update_one({'_id': member.id}, {'$set': cacheData})
-                dbQueue.append(dbData)
-                await asyncio.sleep(0.01)
+        logging.info('[Cache] Performing initial database synchronization')
+        guildCount = len(NS.members)
+        userCount = 0
+        for member in NS.members:
+            userCount += 1
+            await asyncio.sleep(0.01)
+            logging.info(f'[Cache] Syncronizing user {userCount}/{guildCount}')
+            doc = db.find_one({'_id': member.id})
+            if not doc:
+                await store_user(member)
+                continue
 
-        await asyncio.gather(cache_fetch_loop())
-        preCache = copy.deepcopy(userCache) # Initialize starting member cache
-        #logging.info(f'start usercache {userCache}')
-        #logging.info(f'start precache {preCache}')
+            roleList = []
+            for role in member.roles:
+                roleList.append(role.id)
+
+            if roleList == doc['roles']:
+                continue
+
+            db.update_one({'_id': member.id}, {'$set': {
+                'roles': roleList
+            }})
+
+        logging.info('[Cache] Inital database syncronization complete')
         READY = True
 
-    dbQueue = list(filter(None, dbQueue))
-    if dbQueue: # New records to insert
-        db.insert_many(dbQueue)
-
-    logging.info('Bot is fully initialized')
+        logging.info(f'Bot is fully initialized taking {int(time.time()) - startTime}')
 
 @bot.event
 async def on_resume():
@@ -190,7 +111,6 @@ async def on_resume():
 
 @bot.event
 async def on_member_join(member):
-    global userCache
     await bot.wait_until_ready()
     db = mclient.fil.users
     doc = db.find_one({'_id': member.id})
@@ -198,20 +118,7 @@ async def on_member_join(member):
 
     if not doc:
         restored = False
-        for x in member.roles:
-            if x.id == member.guild.id: # We don't want @everyone in the role list. Grab any roles immediately assigned before payload
-                continue
-
-            roleList.append(x.id)
-
-        userData = {
-            '_id': member.id,
-            'messages': 0,
-            'last_message': None,
-            'roles': roleList,
-            'punishments': []
-        }
-        userCache.append(userData)
+        await store_user(member)
 
     else:
         if doc['roles']:
@@ -249,7 +156,6 @@ async def on_member_remove(member):
 
 @bot.event
 async def on_message(message):
-    global userCache
     await bot.wait_until_ready()
     if message.author.bot:
         return
@@ -262,11 +168,10 @@ async def on_message(message):
         logging.debug(f'Not READY. Delaying message {message.id}')
         await asyncio.sleep(1)
 
-    for obj in userCache:
-        if obj['_id'] == message.author.id:
-            obj['messages'] += 1
-            obj['last_message'] = int(time.time())
-            break
+    db = mclient.fil.users
+    doc = db.find_one_and_update({'_id': message.author.id}, {'$inc': {'messages': 1}, '$set': {'last_message': int(time.time())}})
+    if not doc:
+        await store_user(message.author, 1)
 
     await bot.process_commands(message) # Continue commands
 
