cdc479d099b23c4e81babfe4bc3d57d4a0a088f2
Matthew Cohen <matt@mattbsg.xyz>
2019-07-06 00:13:32 -0400

    Message archiving!

--- utils.py
+++ utils.py
@@ -16,6 +16,30 @@ mclient = pymongo.MongoClient(
 
 archiveHeader = '# Message archive for "#{0.name}" ({0.id}) in guild "{1.name}" ({1.id})\n# Format:\n[date + time] Member ID/Message ID/Username - Message content\n----------------\n'
 
+async def message_archive(archive: typing.Union[discord.Message, list]):
+    db = mclient.fil.archive
+    if type(archive) != list:
+        # Single message to archive
+        archive = [archive]
+
+    body = archiveHeader.format(archive[0].channel, archive[0].guild)
+    archiveID = f'{archive[0].id}-{int(time.time() * 1000)}'
+    messageIDs = []
+
+    for msg in archive:
+        messageIDs.append(msg.id)
+        content = '*No message content could be saved, could be embed or attachment*' if not msg.content else msg.content
+        body += f'[{msg.created_at.strftime("%Y/%M/%d %H:%M:%S UTC")}] ({msg.author.id}/{msg.id}/{str(msg.author)}): {content}\n'
+
+    db.insert_one({
+        '_id': archiveID,
+        'body': body,
+        'messages': messageIDs,
+        'timestamp': int(time.time())
+
+    })
+    return archiveID
+
 async def store_user(member, messages=0):
     db = mclient.fil.users
     roleList = []
