1ceefcc16abe02ec6df97030f262d61d3754d8c0
Matthew Cohen <matt@mattbsg.xyz>
2019-07-11 23:59:29 -0400

    Rewrite warn command

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -42,57 +42,60 @@ class Moderation(commands.Cog):
         await self.modLogs.send(embed=embed)
         return await ctx.send(':heavy_check_mark: User banned')
 
-    @commands.command(name='warn')
+    @commands.group(name='warn', invoke_without_command=True)
     @commands.has_any_role(config.moderator, config.eh)
     async def _warning(self, ctx, member: discord.Member, *, reason):
-        tier1 = ctx.guild.get_role(config.warnTier1)
-        tier2 = ctx.guild.get_role(config.warnTier2)
-        tier3 = ctx.guild.get_role(config.warnTier3)
-
+        db = mclient.bowser.puns
         warnLevel = 0
-        Roles = member.roles
-        for role in Roles:
-            if role == tier3:
-                return await ctx.send(f':warning: {member.name}#{member.discriminator} is already at the highest warn tier!')
-        
-            elif role == tier2:
-                warnLevel = 2
+        tierLevel = {
+            0: ctx.guild.get_role(config.warnTier1),
+            1: ctx.guild.get_role(config.warnTier2),
+            2: ctx.guild.get_role(config.warnTier3)
+        }
+        embedColor = {
+            0: discord.Color(0xFFFA1C),
+            1: discord.Color(0xFF9000),
+            2: discord.Color(0xD0021B)
+        }
+        warnText = {
+            0: 'First warning',
+            1: 'Second warning',
+            2: 'Third warning'
+        }
 
-                embed = discord.Embed(color=discord.Color(0xD0021B), timestamp=datetime.datetime.utcnow())
-                embed.set_author(name=f'Third Warning | {str(member)}')
-                embed.add_field(name='User', value=f'<@{member.id}>', inline=True)
-                embed.add_field(name='Moderator', value=f'<@{ctx.author.id}>', inline=True)
-                embed.add_field(name='Reason', value=reason)
+        puns = db.find_one({'user': member.id, 'active': True, 'type': {
+                    '$in': [
+                        'tier1',
+                        'tier2',
+                        'tier3'
+                    ]
+                }
+            }
+        )
+        if puns: # Active punishments, give tier 2/3
+            if puns['type'] == 'tier3':
+                return await ctx.send(f'{config.redTick} That user is already warn tier 3')
 
-                await utils.issue_pun(member.id, ctx.author.id, 'tier3', reason=reason)
-                await member.remove_roles(tier2, reason='Warn action performed by moderator')
-                await member.add_roles(tier3, reason='Warn action performed by moderator')
-        
-            elif role == tier1:
-                warnLevel = 1
-            
-                embed = discord.Embed(color=discord.Color(0xFF9000), timestamp=datetime.datetime.utcnow())
-                embed.set_author(name=f'Second Warning | {str(member)}')
-                embed.add_field(name='User', value=f'<@{member.id}>', inline=True)
-                embed.add_field(name='Moderator', value=f'<@{ctx.author.id}>', inline=True)
-                embed.add_field(name='Reason', value=reason)
-                
-                await utils.issue_pun(member.id, ctx.author.id, 'tier2', reason=reason)
-                await member.remove_roles(tier1, reason='Warn action performed by moderator')
-                await member.add_roles(tier2, reason='Warn action performed by moderator')
-    
-        if warnLevel == 0:
+            db.update_one({'_id': puns['_id']}, {'$set': {
+                'active': False
+            }})
+            warnLevel = 2 if puns['type'] == 'tier2' else 1
 
-            embed = discord.Embed(color=discord.Color(0xFFFA1C), timestamp=datetime.datetime.utcnow())
-            embed.set_author(name=f'First Warning | {str(member)}')
-            embed.add_field(name='User', value=f'<@{member.id}>', inline=True)
-            embed.add_field(name='Moderator', value=f'<@{ctx.author.id}>', inline=True)
-            embed.add_field(name='Reason', value=reason)
+        embed = discord.Embed(color=embedColor[warnLevel], timestamp=datetime.datetime.utcnow())
+        embed.set_author(name=f'{warnText[warnLevel]} | {str(member)}')
+        embed.add_field(name='User', value=f'<@{member.id}>', inline=True)
+        embed.add_field(name='Moderator', value=f'<@{ctx.author.id}>', inline=True)
+        embed.add_field(name='Reason', value=reason)
 
-            await utils.issue_pun(member.id, ctx.author.id, 'tier1', reason=reason)
-            await member.add_roles(tier1, reason='Warn action performed by moderator')
-    
+        for role in member.roles:
+            if role in [tierLevel[0], tierLevel[1], tierLevel[2]]:
+                await member.remove_roles(role, reason='Warn action performed by moderator')
+
+        await member.add_roles(tierLevel[warnLevel], reason='Warn action performed by moderator')
+        await utils.issue_pun(member.id, ctx.author.id, f'tier{warnLevel + 1}', reason)
+
         await self.modLogs.send(embed=embed)
+        return await ctx.send(f'{config.greenTick} {str(member)} ({member.id}) has been successfully warned; they are now tier {warnLevel + 1}')
 
         return await ctx.send(f':heavy_check_mark: Issued a Tier {warnLevel + 1} warning to {str(member)}')
 
