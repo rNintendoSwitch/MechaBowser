rNintendoSwitch/MechaBowser

commit 2c760e5ed3515e21794c50a19c723a9a2bb1a859

Search for em dashes in friend code regex

cogs/social.py
@@ -33,7 +33,7 @@ mclient = pymongo.MongoClient(
 class SocialFeatures(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.friendcodeRe = re.compile(r'(?:sw)?-?(\d{4})[ -]?(\d{4})[ -]?(\d{4})', re.I)
+        self.friendcodeRe = re.compile(r'(?:sw)?[ -\u2014]?(\d{4})[ -\u2014]?(\d{4})[ -\u2014]?(\d{4})', re.I) # \u2014 = em dash
         self.inprogressEdits = {}
         self.letterCodepoints = ['1f1e6', '1f1e7', '1f1e8', '1f1e9', '1f1ea', '1f1eb', '1f1ec', '1f1ed', '1f1ee', '1f1ef', '1f1f0', '1f1f1', '1f1f2', '1f1f3', '1f1f4', '1f1f5', '1f1f6', '1f1f7', '1f1f8', '1f1f9', '1f1fa', '1f1fb', '1f1fc', '1f1fd', '1f1fe', '1f1ff']
 
@@ -582,7 +582,7 @@ class SocialFeatures(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         content = re.sub(r'(<@!?\d+>)', '', message.content)
-        code = re.search(r'(?:sw)?-?(\d{4})[ -](\d{4})[ -](\d{4})', content)
+        code = re.search(r'(?:sw)?[ -\u2014]?(\d{4})[ -\u2014](\d{4})[ -\u2014](\d{4})', content) # \u2014 = em dash
 
         if not code: return
         if message.channel.id not in [config.commandsChannel, config.voiceTextChannel]:

commit 282597640c5762feb5bb73fa3913bbdadb853a10

Fix friendcode regex issues, refactor
- Fixes issue where regex was range (introduced in 2c760e5)
- Fixes issue where chat filter was case sensitive ("sw", edge case)
- Refactors regex definitions to be adjacent lines

cogs/social.py
@@ -33,9 +33,12 @@ mclient = pymongo.MongoClient(
 class SocialFeatures(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.friendcodeRe = re.compile(r'(?:sw)?[ -\u2014]?(\d{4})[ -\u2014]?(\d{4})[ -\u2014]?(\d{4})', re.I) # \u2014 = em dash
         self.inprogressEdits = {}
         self.letterCodepoints = ['1f1e6', '1f1e7', '1f1e8', '1f1e9', '1f1ea', '1f1eb', '1f1ec', '1f1ed', '1f1ee', '1f1ef', '1f1f0', '1f1f1', '1f1f2', '1f1f3', '1f1f4', '1f1f5', '1f1f6', '1f1f7', '1f1f8', '1f1f9', '1f1fa', '1f1fb', '1f1fc', '1f1fd', '1f1fe', '1f1ff']
+        
+        # Friendcode regex for profile editor (lenient) and chat filter (matches separators between digits), \u2014 = em-dash
+        self.friendcodeProfileRe = re.compile(r'(?:sw)?[ \-\u2014]?(\d{4})[ \-\u2014]?(\d{4})[ \-\u2014]?(\d{4})', re.I)
+        self.friendcodeFilterRe = re.compile(r'(?:sw)?[ \-\u2014]?(\d{4})[ \-\u2014](\d{4})[ \-\u2014](\d{4})', re.I)
 
     @commands.group(name='profile', invoke_without_command=True)
     async def _profile(self, ctx, member: typing.Optional[discord.Member]):
@@ -287,7 +290,7 @@ class SocialFeatures(commands.Cog):
                 await message.channel.send('I\'ve gone ahead and reset your setting for **friend code**')
                 return True
 
-            code = re.search(self.friendcodeRe, content)
+            code = re.search(self.friendcodeProfileRe, content)
             if code: # re match
                 friendcode = f'SW-{code.group(1)}-{code.group(2)}-{code.group(3)}'
                 db.update_one({'_id': ctx.author.id}, {'$set': {'friendcode': friendcode}})
@@ -582,7 +585,7 @@ class SocialFeatures(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         content = re.sub(r'(<@!?\d+>)', '', message.content)
-        code = re.search(r'(?:sw)?[ -\u2014]?(\d{4})[ -\u2014](\d{4})[ -\u2014](\d{4})', content) # \u2014 = em dash
+        code = re.search(self.friendcodeFilterRe, content) 
 
         if not code: return
         if message.channel.id not in [config.commandsChannel, config.voiceTextChannel]: