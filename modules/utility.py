cab7da579ba8b811d59e18374bd2a29d509ebc2a
Ian M <theianm2002@gmail.com>
2020-12-13 13:07:12 -0500

    Add tag list command

--- modules/utility.py
+++ modules/utility.py
@@ -508,12 +508,42 @@ class ChatControl(commands.Cog, name='Utility Commands'):
             return await ctx.send(embed=embed)
 
         else:
-            tagList = []
-            for x in db.find({'active': True}):
-                tagList.append(x['_id'])
+            await self._tag_list(ctx)
 
-            embed = discord.Embed(title='Tag List', description='Here is a list of tags you can access:\n\n' + ', '.join(tagList))
+    @_tag.command(name='list')
+    async def _tag_list(self, ctx):
+        db = mclient.bowser.tags
+        embed_title = 'Tag List'
+        embed_desc = 'Here is a list of tags you can access:'
+
+        tagList = []
+        for tag in db.find({'active': True}):
+            description = tag['desc'] if 'desc' in tag else ''
+            tagList.append((tag['_id'], description))
+
+        if not tagList: return await ctx.send('{config.redTick} This server has no tags!')
+
+        if ctx.invoked_with == 'tag': # Called from the !tag command instead of !tag list, so we print the simple list
+            embed = discord.Embed(title=embed_title, description=f'{embed_desc}\n\n' + ', '.join( [x[0] for x in tagList] ))
+            embed.set_footer(text=f'Use {ctx.prefix}tag list to view tag descriptions')
             return await ctx.send(embed=embed)
+
+        else:
+            # If the command is being not being run in commands channel, they must be a mod or helpful user to run it.
+            if ctx.channel.id != config.commandsChannel:
+                if not (ctx.guild.get_role(config.modemeritus) in ctx.author.roles or ctx.guild.get_role(config.helpfulUser) in ctx.author.roles):
+                    return await ctx.send(f'{config.redTick} {ctx.author.mention} Please use this command in <#{config.commandsChannel}>, not {ctx.channel.mention}', delete_after=15)
+
+            longest_name = len(max([x[0] for x in tagList], key=len))
+            lines = []
+
+            for tag in tagList:
+                name = tag[0].ljust(longest_name)
+                desc = tag[1] if tag[1] else '*No description*'
+                lines.append(f'`{name}` {desc}')
+
+            fields = utils.convert_list_to_fields(lines, codeblock=False)
+            return await utils.send_paginated_embed(self.bot, ctx.channel, fields, owner=ctx.author, title=embed_title, description=embed_desc, page_character_limit=1500)
 
     @_tag.command(name='edit')
     @commands.has_any_role(config.moderator, config.helpfulUser)
@@ -522,7 +552,7 @@ class ChatControl(commands.Cog, name='Utility Commands'):
         name = name.lower()
         tag = db.find_one({'_id': name})
         await ctx.message.delete()
-        if name in ['edit', 'delete', 'source']:
+        if name in ['list', 'edit', 'delete', 'source']:
             return await ctx.send(f'{config.redTick} You cannot use that name for a tag', delete_after=10)
 
         if tag:
@@ -687,6 +717,7 @@ class ChatControl(commands.Cog, name='Utility Commands'):
     @_roles.error
     @_roles_set.error
     @_tag.error
+    @_tag_list.error
     @_tag_create.error
     @_tag_delete.error
     @_tag_source.error
