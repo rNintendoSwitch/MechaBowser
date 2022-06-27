9a5fdb52e511db6622c7712b5bc01794bc4eb518
Ian M <6500391+7bitlyrus@users.noreply.github.com>
2022-06-27 15:35:52 -0400

    moderation.py: fix strike_set mod ctx check

--- modules/moderation.py
+++ modules/moderation.py
@@ -831,6 +831,7 @@ class Moderation(commands.Cog, name='Moderation Commands'):
                     f'({activeStrikes} - {removedStrikes}) {error}'
                 )
 
+            else:
                 return await ctx.message.delete()
 
     async def cog_command_error(self, ctx: commands.Context, error: commands.CommandError):
