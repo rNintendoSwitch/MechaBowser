62f11b65fadcf480a980f3c7d1765ef6d274993b
Ian M <theianm2002@gmail.com>
2022-09-23 19:33:42 -0400

    Add third team for splatfest (untested)

--- events/splatfest.py
+++ events/splatfest.py
@@ -13,6 +13,7 @@ class Splatfest(commands.Cog):
         self.ACTIVE = False
         self.team1 = None
         self.team2 = None
+        self.team3 = None
 
     @commands.has_any_role(config.moderator, config.eh)
     @commands.group(name='splatfest', invoke_without_command=True)
@@ -99,6 +100,52 @@ class Splatfest(commands.Cog):
                 except ValueError:
                     await ctx.send(f'{config.redTick} That is not a valid role, please send a valid role ID')
 
+            # Team 3 Logic
+            await ctx.send(
+                f'Team 2 has been set as {_team2}! What is the name of team 3? (type `skip` for only 2 teams)?'
+            )
+            _team3 = await self.bot.wait_for('message', check=check, timeout=60.0)
+            _team3 = _team3.content
+            if _team3 == 'cancel':
+                return await ctx.send('Canceled setup. Rerun command to try again')
+
+            if _team3 == 'skip':
+                await ctx.send(f'Team 3 has been skipped.')
+                _team3 = None
+            else:
+                msg = await ctx.send(f'What emote represents team {_team3}?')
+                while True:
+                    try:
+                        _team3_emote = await self.bot.wait_for('message', check=check, timeout=60.0)
+                        _team3_emote = _team3_emote.content
+                        if _team3_emote == 'cancel':
+                            return await ctx.send('Canceled setup. Rerun command to try again')
+                        await msg.add_reaction(_team3_emote)
+                        await msg.remove_reaction(_team3_emote, self.bot.user)
+                        break
+
+                    except (discord.NotFound, discord.InvalidArgument, discord.HTTPException):
+                        await ctx.send(
+                            f'{config.redTick} That is not a valid emoji, please send a valid unicode or custom emoji'
+                        )
+
+                await ctx.send(f'What role represents team {_team3}? (Please send the ID)')
+                while True:
+                    try:
+                        _role = await self.bot.wait_for('message', check=check, timeout=60.0)
+                        _role = _role.content
+                        if _role == 'cancel':
+                            return await ctx.send('Canceled setup. Rerun command to try again')
+                        _team3_role = ctx.guild.get_role(int(_role))
+                        if not _team3_role:
+                            raise ValueError
+                        break
+
+                    except ValueError:
+                        await ctx.send(f'{config.redTick} That is not a valid role, please send a valid role ID')
+
+                await ctx.send(f'Team 3 has been set as {_team3}!')
+
             # Channel logic
             await ctx.send(f'What channel should team choosing be in? (Please send the ID)')
             while True:
@@ -125,10 +172,15 @@ class Splatfest(commands.Cog):
 
         self.team1 = {'name': _team1, 'emote': _team1_emote, 'role': _team1_role.id}
         self.team2 = {'name': _team2, 'emote': _team2_emote, 'role': _team2_role.id}
+        if _team3 is None:
+            self.team3 = None
+        else:
+            self.team3 = {'name': _team3, 'emote': _team3_emote, 'role': _team3_role.id}
+
         self.channel = _channel.id
         self.ACTIVE = True
         await ctx.send(
-            f'{config.greenTick} Team 2 has been set as {_team2}. The event is now activated! To end the event, run `{ctx.prefix}splatfest end`'
+            f'{config.greenTick} The event is now activated! To end the event, run `{ctx.prefix}splatfest end`'
         )
 
     @commands.has_any_role(config.moderator, config.eh)
@@ -138,6 +190,7 @@ class Splatfest(commands.Cog):
         self.channel = None
         self.team1 = None
         self.team2 = None
+        self.team3 = None
         await ctx.send(f'{config.greenTick} Splatfest ended!')
 
     @commands.Cog.listener()
@@ -147,6 +200,12 @@ class Splatfest(commands.Cog):
             team1Role = message.guild.get_role(self.team1['role'])
             team2Emote = re.compile(f'({self.team2["emote"]})+', re.I)
             team2Role = message.guild.get_role(self.team2['role'])
+            if self.team3:
+                team3Emote = re.compile(f'({self.team3["emote"]})+', re.I)
+                team3Role = message.guild.get_role(self.team3['role'])
+            else:
+                team3Emote = None
+                team3Role = None
 
             if re.search(team1Emote, message.content) and re.search(team2Emote, message.content):
                 # If the user puts both roles in the same message, toss it
@@ -156,6 +215,9 @@ class Splatfest(commands.Cog):
                 if re.search(team1Emote, message.content):
                     if team2Role in message.author.roles:
                         await message.author.remove_roles(team2Role)
+
+                    if self.team3 and team3Role in message.author.roles:
+                        await message.author.remove_roles(team3Role)
 
                     if team1Role not in message.author.roles:
                         msg = await message.channel.send(
@@ -169,6 +231,9 @@ class Splatfest(commands.Cog):
                     if team1Role in message.author.roles:
                         await message.author.remove_roles(team1Role)
 
+                    if self.team3 and team3Role in message.author.roles:
+                        await message.author.remove_roles(team3Role)
+
                     if team2Role not in message.author.roles:
                         msg = await message.channel.send(
                             f'<@{message.author.id}> You are now registered as a member of Team {self.team2["name"]}',
@@ -176,6 +241,21 @@ class Splatfest(commands.Cog):
                         )
                         await msg.delete(delay=5.0)
                         await message.author.add_roles(team2Role)
+
+                elif self.team3 and re.search(team3Emote, message.content):
+                    if team1Role in message.author.roles:
+                        await message.author.remove_roles(team1Role)
+
+                    if team2Role in message.author.roles:
+                        await message.author.remove_roles(team2Role)
+
+                    if team3Role not in message.author.roles:
+                        msg = await message.channel.send(
+                            f'<@{message.author.id}> You are now registered as a member of Team {self.team3["name"]}',
+                            delete_after=10,
+                        )
+                        await msg.delete(delay=5.0)
+                        await message.author.add_roles(team3Role)
 
             except (discord.Forbidden, discord.HTTPException):
                 raise
