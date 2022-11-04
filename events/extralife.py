a8b7eb8c82b95c1bc7c7220fa834e99889eac128
Matthew Cohen <matt@mattbsg.xyz>
2022-11-04 18:09:36 -0400

    Auto/manual perks granting and adds incentive field

--- events/extralife.py
+++ events/extralife.py
@@ -1,9 +1,15 @@
 import logging
+import re
 from datetime import datetime
+from typing import List
 
 import discord
+import pymongo
 import requests
 from discord.ext import commands, tasks
+
+import config
+import tools
 
 
 class ExtraLife(commands.Cog):
@@ -22,8 +28,22 @@ class ExtraLife(commands.Cog):
         # Role adding consts
         self.CHAT_CHANNEL = 654018662860193830
         self.CHAT_ROLE = 927032355032547349
+        self.DONOR_ROLE = 927032536704626699
+
+        # Trophy and Background consts
+        self.TROPHY = 'extra-life-2022'
+        # self.BACKGROUND = 'extra-life'
+        self.BACKGROUND = 'extra-life'
+
+        # Donation incentive ID consts
+        self.INCENTIVES = {
+            'B1002B58-0242-02AC-B7D48CC7C6F93173': 'Sticker Sheets and Discord Perks',
+            'B2CA8080-9217-DD59-AB885C0C9E4BC687': 'Enamel Pin, Sticker Sheet, and Discord Perks',
+        }
+
         ################################################################################################################################
 
+        self.mclient = pymongo.MongoClient(config.mongoHost, username=config.mongoUser, password=config.mongoPass)
         self.bot = bot
         self.guild = self.bot.get_guild(self.GUILD)
         self.extra_life_admin = self.guild.get_channel(self.EXTRA_LIFE_ADMIN)
@@ -31,6 +51,7 @@ class ExtraLife(commands.Cog):
         self.general = self.guild.get_channel(self.GENERAL)
         self.donations = self.guild.get_channel(self.DONATIONS)
         self.chatRole = self.guild.get_role(self.CHAT_ROLE)
+        self.donorRole = self.guild.get_role(self.DONOR_ROLE)
         self.lastDonationID = None
 
         self.donation_check.start()
@@ -43,6 +64,50 @@ class ExtraLife(commands.Cog):
 
         self.lastDonationID = string
         return await ctx.send(content=f'Last donation id set to `{string}`')
+
+    @commands.group(name='elperks', invoke_without_command=False)
+    async def _perks(self, ctx):
+        pass
+
+    @_perks.command(name='grant')
+    @commands.check_any(commands.is_owner(), commands.has_guild_permissions(view_audit_log=True))
+    async def perks_grant(self, ctx, members: commands.Greedy[tools.ResolveUser]):
+        errors = 0
+        msg = await ctx.send(f'{config.loading} Granting Extra Life perks to {len(members)} member(s)...')
+        for member in members:
+            try:
+                await self._assign_properties(member)
+
+            except ValueError:
+                errors += 1
+
+        if errors == len(members):
+            return await msg.edit(content=f'{config.redTick} Extra Life perks granted to 0 members')
+
+        else:
+            return await msg.edit(
+                content=f'{config.greenTick} Extra Life perks granted to {len(members) - errors}/{len(members)} member(s).'
+            )
+
+    @_perks.command(name='revoke')
+    @commands.check_any(commands.is_owner(), commands.has_guild_permissions(view_audit_log=True))
+    async def perks_revoke(self, ctx, members: commands.Greedy[tools.ResolveUser]):
+        errors = 0
+        msg = await ctx.send(f'{config.loading} Revoking Extra Life perks to {len(members)} member(s)...')
+        for member in members:
+            try:
+                await self._remove_properties(member)
+
+            except ValueError:
+                errors += 1
+
+        if errors == len(members):
+            return await msg.edit(content=f'{config.redTick} Extra Life perks revoked from 0 members')
+
+        else:
+            return await msg.edit(
+                content=f'{config.greenTick} Extra Life perks revoked from {len(members) - errors}/{len(members)} member(s).'
+            )
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -76,6 +141,24 @@ class ExtraLife(commands.Cog):
                 break
 
             donor_name = 'Anonymous' if not 'displayName' in donation else donation['displayName']
+            match = re.match(r'([\s\S]+)#(\d{4})', donor_name)
+            if match:
+                # Donor name format matches a Discord username
+                member = discord.utils.find(
+                    lambda m: m.name == match.group(1) and m.discriminator == match.group(2), self.guild.members
+                )
+                if member:
+                    if self.donorRole not in member.roles:
+                        try:
+                            await self._assign_properties(member)
+
+                        except ValueError:
+                            await self.extra_life_admin.send(
+                                f':warning: An error occured while attempting to grant donation benefits to `{donor_name}`, they already have the background or trophy'
+                            )
+
+                    donor_name = donor_name + f' ({member.mention})'
+
             donation_time = datetime.strptime(
                 donation['createdDateUTC'], '%Y-%m-%dT%H:%M:%S.%f%z'
             )  # eg 2020-11-07T06:00:07.327+0000
@@ -91,6 +174,10 @@ class ExtraLife(commands.Cog):
             if 'message' in donation:
                 embed.add_field(name="Message", value=donation['message'], inline=False)
 
+            if 'incentiveID' in donation and donation['incentiveID'] in self.INCENTIVES.keys():
+                # If an incentive, double check it matches what we know
+                embed.add_field(name="Incentive claimed", value=self.INCENTIVES[donation['incentiveID']])
+
             embed.add_field(name="\uFEFF", value=self.FOOTER_LINKS, inline=False)  # ZERO WIDTH NO-BREAK SPACE (U+FEFF)
             donation_embeds.append(embed)
             logging.info(f'Sending donation {donation["donationID"]} from {donor_name}')
@@ -103,6 +190,16 @@ class ExtraLife(commands.Cog):
             await self.donations.send(embed=embed)
 
         self.lastDonationID = donations[0]['donationID']
+
+    async def _assign_properties(self, member: discord.Member):
+        await member.add_roles(self.donorRole)
+        await tools.commit_profile_change(self.bot, member, 'trophy', self.TROPHY)
+        await tools.commit_profile_change(self.bot, member, 'background', self.BACKGROUND)
+
+    async def _remove_properties(self, member: discord.Member):
+        await member.remove_roles(self.donorRole)
+        await tools.commit_profile_change(self.bot, member, 'trophy', self.TROPHY, revoke=True)
+        await tools.commit_profile_change(self.bot, member, 'background', self.BACKGROUND, revoke=True)
 
     def cog_unload(self):
         self.donation_check.cancel()  # pylint: disable=no-member
